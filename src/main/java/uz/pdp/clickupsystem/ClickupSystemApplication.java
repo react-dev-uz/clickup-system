a0a2ae66f2f223abc70510261da72cd01ad834ea
Dilshod Fayzullayev <fullstack.dev.uz@gmail.com>
2023-02-13 16:43:56 +0500

    :tada: Create ClickUp System Spring application

---
+++ src/main/java/uz/pdp/clickupsystem/ClickupSystemApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.clickupsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickupSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickupSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/clickupsystem/ClickupSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.clickupsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickupSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
